199f421ee0870616c4fb21fa4e80142ebd482a47
Lee Sheen <leesheen@outlook.com>
2016-01-21 14:49:23 +0800

    Reference material

---
+++ other/tmp.js
@@ -0,0 +1,43 @@
+{
+    "translation": [
+        "\u674e"
+    ],
+    "basic": {
+        "us-phonetic": "li",
+        "phonetic": "li\u02d0",
+        "uk-phonetic": "li\u02d0",
+        "explains": [
+            "n. \u4fdd\u62a4\uff1b\u80cc\u98ce\u5904",
+            "adj. \u4fdd\u62a4\u7684\uff1b\u5e87\u836b\u7684\uff1b\u907f\u98ce\u7684",
+            "n. (Lee)\u4eba\u540d\uff1b(\u671d)\u674e\uff1b(\u4e2d)\u674e(\u666e\u901a\u8bdd\u00b7\u5a01\u59a5\u739b)\uff1b(\u67ec)\u5229\uff1b(\u4e2d)\u674e(\u5e7f\u4e1c\u8bdd\u00b7\u5a01\u59a5\u739b)"
+        ]
+    },
+    "query": "lee",
+    "errorCode": 0,
+    "web": [
+        {
+            "value": [
+                "\u674e",
+                "\u5e87\u62a4\u6240",
+                "\u82f1\u56fd"
+            ],
+            "key": "Lee"
+        },
+        {
+            "value": [
+                "\u674e\u94ed\u987a",
+                "\u514b\u91cc\u65af\u6258\u5f17\u00b7\u674e",
+                "\u514b\u91cc\u65af\u6258\u5f17\u00b7\u674e"
+            ],
+            "key": "Christopher Lee"
+        },
+        {
+            "value": [
+                "\u65af\u6d3e\u514b\u00b7\u674e",
+                "\u53f2\u6d3e\u514b\u00b7\u674e",
+                "\u65af\u6d3e\u514b"
+            ],
+            "key": "Spike Lee"
+        }
+    ]
+}

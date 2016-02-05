ee2da96211f8de0e1ac907da381109da4cb35c8a
Lee Sheen <leesheen@outlook.com>
2016-02-06 03:08:05 +0800

    A description for error number

---
+++ src/errno.h
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  errno.h
+ *
+ *    Description:  A description for dict error number.
+ *
+ *        Version:  1.0
+ *        Created:  02/05/2016 09:00:10 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Lee Sheen (lee), leesheen@outlook.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef __ERRNO_H__
+#define __ERRNO_H__
+
+#define ERRNO_SUCCESS	0
+
+#define ERRNO_INIT		1
+#define ERRNO_EXEC		2
+#define ERRNO_EXIT		3
+
+
+#endif

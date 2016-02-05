4e001ff5763fb369aca0a8d6382d294ba88c9ec0
Lee Sheen <leesheen@outlook.com>
2016-02-06 03:07:17 +0800

    优化了代码结构

--- src/dict.c
+++ src/dict.c
@@ -12,55 +12,241 @@
  *
  *         Author:  Lee Sheen (leesheen@outlook.com), 
  *   Organization:  
- *	　注： 词典结果只支持中英互译，翻译结果支持英日韩法俄西到中文的翻译以及中文
- *	到英语的翻译
- *	errorCode：
- *	　0 - 正常
- *	　20 - 要翻译的文本过长
- *	　30 - 无法进行有效的翻译
- *	　40 - 不支持的语言类型
- *	　50 - 无效的key
- *	　60 - 无词典结果，仅在获取词典结果生效
  *
  * =====================================================================================
  */
 
+#define _GNU_SOURCE
 #include "dict.h"
 
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#undef _GNU_SOURCE
 
-int main(int argc, char *argv[])
+static void print_help()
 {
-	FILE	*resout = stdout;
-	int		i;
-	char 	trans_chars[TRANS_CHARS_BUF] = {0};
-	char 	trans_char[TRANS_CHARS_BUF] = {0};
 
-	if (argc < 2)
+	printf("Usage: dict [options] [value] ...\n");
+	printf("<value> has to be either integral(raw mode) or decimal(percent mode) depending on the specified value mode.\n");
+	printf("<options> can be any of the following:\n\n");
+
+	printf("Operations:\n");
+	printf("  -H -h:\tPrints this help and exits\n");
+
+	printf("\n 请输出需要查询的关键字，比如\n\n ~ dict example\n");
+	printf("\n 如果英文段落中有“'”(单引号)等符号，可以在需要翻译的语句前后加半角双引号，比如\n\n ~ dict \"I'm a programmer.\"\n\n");
+
+	return ;
+}
+
+static void print_version()
+{
+
+	printf("--------\n\n\n\n\n");
+	
+}
+
+static DICT_BOOL dict_choose_api(DICT_STR *dict_str_tmp, char *optarg)
+{
+	int i;
+	DICT_BOOL is_print_help = TRUE;
+	
+	dict_str_tmp->is_vaild = FALSE;
+
+	for (i=0; i<DICT_API_NAME_NUM; i++)
+		if (NULL !=	strcasestr(g_dict_api_name_array[i], optarg))
+		{
+			dict_str_tmp->dict_api = i;
+			dict_str_tmp->is_vaild = TRUE;
+			is_print_help = FALSE;
+		}
+
+	return is_print_help;
+}
+
+
+static DICT_BOOL dict_arg_init(DICT_STR *dict_str_tmp,
+		int argc, char *argv[])
+{
+	int i = 0;
+	int opt_res	= 0;
+	int content_len = 0;
+	DICT_BOOL is_print_version = FALSE;
+	DICT_BOOL is_print_help = FALSE;
+
+	/* 可选参数默认选项 */
+	dict_str_tmp->is_vaild	=	TRUE;
+	dict_str_tmp->dict_api	=	API_YOUDAO;		/* TODO 从配置文件中导入*/
+	dict_str_tmp->res_in	=	stdin;
+	dict_str_tmp->res_out	=	stdout;
+
+	while (-1 != (opt_res = getopt(argc, argv, DICT_OPTSTRING)))
 	{
-		for (i=0; i<30; i++)
-			printf("-");
-		printf("\n 请输出需要查询的关键字，比如\n\n ~ dict example\n");
-		printf("\n 如果英文段落中有“'”(单引号)等符号，可以在需要翻译的语句前后加半角双引号，比如\n\n ~ dict \"I'm a programmer.\"\n\n");
-		return 0;
+		switch (opt_res) 
+		{
+			/* 版本 */
+			case 'v':
+			{
+				dict_str_tmp->is_vaild = FALSE;
+				is_print_version = TRUE;
+				break;
+			}
+
+			/* TODO 输入选项 */
+			case 'i':
+			{break;}
+
+			/* TODO 输出选项 */
+			case 'o':
+			{break;}
+
+			/* API选择 <baidu> <youdao> etc...*/
+			case 'a':
+			{
+				is_print_help = dict_choose_api(dict_str_tmp, optarg);
+				break;
+			}
+
+			/* 测试参数 */
+			case 'V':
+			{break;}
+
+			/* 帮助 */
+			case 'H':
+			case 'h':
+			default:
+			{
+				is_print_help = TRUE;
+				dict_str_tmp->is_vaild = FALSE;
+				break;
+			}
+		}
 	}
-	printf("********************************\n\n"); 
+
+	/* 填充content_location */
+	if ((optind == argc) && (is_print_version != TRUE))
+	{
+		is_print_help = TRUE;
+		dict_str_tmp->is_vaild = FALSE;
+	}
+	else
+		dict_str_tmp->content_location = optind;
+
+	/* 填充content_len */
+	for (i=optind; i<argc; i++)
+	{
+		content_len += strlen(argv[i]);
+		content_len += 1;
+	}
+	dict_str_tmp->content_len = content_len;
+
+
+	if (is_print_version == TRUE)
+		print_version();
+	if (is_print_help == TRUE)
+		print_help();
+	
+	return dict_str_tmp->is_vaild;
+}
+
+static void dict_str_add_content(DICT_STR * dict_str_tmp, 
+		int argc, char *argv[])
+{
+	int i;
+	int len			= dict_str_tmp->content_len;
+	int location	= dict_str_tmp->content_location;
+
+	unsigned char *content = (unsigned char *)malloc(len);
+
+	memset(content, 0, len);
 
 	/* Strcat what words needs translate */
-	for (i=1; i<argc; i++)
+	for (i=location; i<argc; i++)
 	{
-		strcat(trans_chars, argv[i]);
-		strcat(trans_chars, " ");
+		strncat(content, argv[i], strlen(argv[i]));
+		strncat(content, " ", 1);
 	}
-	
+	content[len-1] = '\0';
+
+	dict_str_tmp->content = content; 
+
+	return ;
+}
+
+
+static int dict_init(DICT_STR **dict_str_pp, int argc, char *argv[])
+{
+	int ret	= ERRNO_INIT;
+	int trans_words_len = 0;
+
+	DICT_STR  dict_str_tmp;
+
+	dict_arg_init(&dict_str_tmp, argc, argv);
+	if (dict_str_tmp.is_vaild == TRUE)
+	{
+		dict_str_add_content(&dict_str_tmp, argc, argv);
+
+		*dict_str_pp = (DICT_STR *)malloc(sizeof (DICT_STR));
+		memcpy(*dict_str_pp, &dict_str_tmp, sizeof (DICT_STR));
+
+		ret = ERRNO_SUCCESS;
+	}
+
+	return ret;
+}
+
+static int dict_exec(DICT_STR *dict_str_p)
+{
+	int ret = 0;
+
 	//baidu_translate(resout, trans_chars);
 //	youdao_translate(resout, trans_chars);
 
-	printf("\n********************************\n"); 
+	return ret;
+}
 
+static int dict_free(DICT_STR *dict_str_p)
+{
+	free(dict_str_p->content);
+	free(dict_str_p);
 	return 0;
 }
 
+int main(int argc, char *argv[])
+{
+	int	ret = 0;
+	DICT_STR *my_dict_str = NULL;
+
+	ret = dict_init(&my_dict_str, argc, argv);
+	if (ERRNO_INIT == ret)
+	{
+		/* 如果没有初始化此结构体，说明用户执行 --help ,
+		 * 并没有出错，所有此处不打印出错信息，直接返回。
+		 * 而对于程序来说，--help或者错误的参数也是程序
+		 * 没有正确执行的一部分，所以main仍返回错误。*/
+		if (NULL != my_dict_str)
+			printf("[DICT]%s: Initialization Failed.", __func__);
+
+		goto err;
+	}
+	
+	ret = dict_exec(my_dict_str);
+	if (ERRNO_EXEC == ret)
+	{
+		printf("[DICT]%s: Execution Failed.", __func__);
+		goto err;
+	}
+
+	ret = dict_free(my_dict_str);
+	if (ERRNO_EXIT == ret)
+	{
+		printf("[DICT]%s: Exit Failed.", __func__);
+		goto err;
+	}
+
+err:
+	return ret;
+}
 
